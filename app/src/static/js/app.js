6dbc28c0ea33f21c0edfbaa0c9b5a74656515bf9
rodoxset <52052722+rodoxset@users.noreply.github.com>
2023-06-05 22:03:03 -0300

    Update app.js- traduzido a linha para pt/br

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,8 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">Você ainda não tem itens para fazer! Adicione um acima!</p>
+                
             )}
             {items.map(item => (
                 <ItemDisplay
